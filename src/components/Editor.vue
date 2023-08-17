1c51d5698352119a1467fc1c1849c8ff30d4e462
Gábor Kriskó <gabor.krisko@gmail.com>
2023-08-17 22:02:57 +0200

    hex viewer

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,4 +1,25 @@
-<template>
-    <div></div>
+<template v-if="state.currentFile">
+    <div ref="container" class="editor"></div>
 </template>
 
+<script setup lang="ts">
+import { state } from '@/state';
+import { onMounted, ref } from 'vue';
+import { container as binaryContainer } from '../binaryData/manager';
+
+const container = ref<HTMLElement>();
+onMounted(()=>{
+    if (container.value){
+        container.value.appendChild(binaryContainer);
+    }
+})
+</script>
+
+<style scoped lang="scss">
+.editor {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+    overflow: hidden;
+}
+</style>
