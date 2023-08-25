9318246bafa81ddf72054a8a87d2db6d7bd962a4
Gábor Kriskó <gabor.krisko@gmail.com>
2023-08-25 10:46:09 +0200

    open multiple files

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -8,15 +8,21 @@
 
 <script setup lang="ts">
 import { TabData } from '@/TabData';
-import { openFile } from '../openFile';
+import { openFiles } from '../openFile';
 import { editor, state } from '@/state';
 import { switchTab } from '@/tabs';
 
 function onOpenFile(){
-    openFile().then(file=>{
-        const tab = new TabData(file.name,file);
-        state.tabs.push(tab);
-        switchTab(tab);
+    openFiles().then(files=>{
+        const tabs = files.map(file=>new TabData(file.name,file));
+        for (let tab of tabs){
+            state.tabs.push(tab);
+        }
+        const last = tabs.at(-1);
+        if (last) {
+            switchTab(last);
+        }
+        
     })
 }
 
