98a1b0f96d0d4993cb22de38f22f2eb9187238a1
kess <git@kess.sh>
2024-01-10 00:19:34 +0100

    composition

--- src/binaryData/interfaces/DataHandler.ts
+++ src/binaryData/interfaces/DataHandler.ts
@@ -2,34 +2,42 @@ import { Editor } from "../editor";
 import { TrackedVar } from "../reactivity";
 import { bytesPerRow } from "../constants";
 import type { DataProvider } from "../dataProvider";
+import { applySubsystem, defineSubsystem, subsystemProps, type SubsystemInterface } from "../composition";
 
 export function getDataProviderRowCount(dataProvider: DataProvider){
     return Math.ceil( dataProvider.size / bytesPerRow );
 }
 
-export interface IDataHandler {
-    initDataHandler: ()=>void
-    dataToRender: TrackedVar<Uint8Array>
-    getPage: (dataProvider: DataProvider, startByte: number)=>Promise<Uint8Array>
-    getBytes: (startByte: number)=>Uint8Array
-    getByte: (i: number)=>number | undefined
-}
+export type IDataHandler = SubsystemInterface<typeof DataHandler>;
+
+export const DataHandler = defineSubsystem({
+    name: "DataHandler",
+    props: subsystemProps<{
+        dataToRender: TrackedVar<Uint8Array>
+    }>(),
+    proto: {
+        initDataHandler(this: Editor){
+            this.dataToRender = new TrackedVar<Uint8Array>(new Uint8Array(0));
+        },
+        async getPage(this: Editor, dataProvider: DataProvider, startByte: number): Promise<Uint8Array> {
+            const length = this.viewportRowCount.value * bytesPerRow;
+           
+            return await dataProvider.readAsync(startByte,length);
+        },
+        getBytes(this: Editor, startByte: number): Uint8Array {
+            const index = startByte - this.intermediateState.value.topRow * bytesPerRow;
+            const buffer = this.dataToRender.value.slice(index, index + bytesPerRow);
+            return new Uint8Array(buffer);
+        },
+        getByte(this: Editor, i: number): number | undefined {
+            return this.dataToRender.value[i];
+        }
+    },
+    init(this: Editor) {
+        
+    }
+})
 
 export function patchDataHandler(){
-    Editor.prototype.initDataHandler = function(this: Editor){
-        this.dataToRender = new TrackedVar<Uint8Array>(new Uint8Array(0));
-    }
-    Editor.prototype.getPage = async function(this: Editor, dataProvider: DataProvider, startByte: number): Promise<Uint8Array> {
-        const length = this.viewportRowCount.value * bytesPerRow;
-       
-        return await dataProvider.readAsync(startByte,length);
-    }
-    Editor.prototype.getBytes = function(this: Editor, startByte: number): Uint8Array {
-        const index = startByte - this.intermediateState.value.topRow * bytesPerRow;
-        const buffer = this.dataToRender.value.slice(index, index + bytesPerRow);
-        return new Uint8Array(buffer);
-    }
-    Editor.prototype.getByte = function(i: number): number | undefined {
-        return this.dataToRender.value[i];
-    }
+    applySubsystem(Editor,DataHandler);
 }
