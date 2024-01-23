3d11103c00811738a96628cb56b95c5b82ab94cd
Gábor Kriskó <gabor.krisko@gmail.com>
2024-01-23 13:18:04 +0100

    move any*Box to *Box.ts

--- src/binaryData/subsystems/RenderingManager/layout/byteBox.ts
+++ src/binaryData/subsystems/RenderingManager/layout/byteBox.ts
@@ -1,5 +1,16 @@
-import { BoundingBox, CachedBoundingBoxes } from "../box";
+import { BoundingBox, CachedBoundingBox, CachedBoundingBoxes } from "../box";
 import type { Layout } from "../layout";
+
+export function anyByteBox(layout: Layout){
+    return new CachedBoundingBox(()=>{
+        return new BoundingBox({
+            outerLeft: 0,
+            outerTop: 0,
+            innerWidth: layout.styles.editorByteWidth * layout.unit,
+            innerHeight: layout.rowHeight * layout.unit,
+        })
+    })
+}
 
 export function byteBox(layout: Layout){
     return new CachedBoundingBoxes((y: number, x: number)=>{
--- src/binaryData/subsystems/RenderingManager/layout/byteCountBox.ts
+++ src/binaryData/subsystems/RenderingManager/layout/byteCountBox.ts
@@ -1,5 +1,24 @@
-import { BoundingBox, CachedBoundingBoxes } from "../box";
+import { BoundingBox, CachedBoundingBox, CachedBoundingBoxes } from "../box";
 import type { Layout } from "../layout";
+
+export function anyByteCountBox(layout: Layout) {
+    return new CachedBoundingBox(()=>{
+        const ctx = layout.ctx;
+        const count = layout.getByteCountDigitCount();
+        ctx.font = layout.styles.getByteCountFont();
+        const text = layout.getPaddedByteCount(count);
+        const size = ctx.measureText(text);
+        const textWidth = size.width;
+        return new BoundingBox({
+            outerLeft: 0,
+            outerTop: 0,
+            innerWidth: textWidth,
+            innerHeight: layout.rowHeight * layout.unit,
+            paddingLeft: 4 * layout.unit,
+            paddingRight: 4 * layout.unit
+        })
+    })
+}
 
 export function byteCountBox(layout: Layout) {
     return new CachedBoundingBoxes((y: number)=>{
