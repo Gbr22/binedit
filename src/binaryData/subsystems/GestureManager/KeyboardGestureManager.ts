34e281501e32ebc4c8761b99d0280947ad3454e7
kess <git@kess.sh>
2025-07-20 16:49:08 +0200

    implement page up/down, home, end

--- src/binaryData/subsystems/GestureManager/KeyboardGestureManager.ts
+++ src/binaryData/subsystems/GestureManager/KeyboardGestureManager.ts
@@ -10,40 +10,61 @@ export class KeyboardGestureManager {
         this.manager = manager;
 
         this.editor.dom.innerContainer.onkeydown = (e)=>{
-            const dir = {
-                x: 0,
-                y: 0,
+            let cursorByteDiff = 0;
+            let scrollByteDiff = 0;
+            {
+                const cursorMove = {
+                    x: 0,
+                    y: 0,
+                }
+                if (e.code == "ArrowUp"){
+                    cursorMove.y = -1;
+                }
+                if (e.code == "ArrowDown"){
+                    cursorMove.y = 1;
+                }
+                if (e.code == "ArrowLeft"){
+                    cursorMove.x = -1;
+                }
+                if (e.code == "ArrowRight"){
+                    cursorMove.x = 1;
+                }
+                cursorByteDiff = cursorMove.y * this.editor.rendering.layout.bytesPerRow + cursorMove.x;
             }
-            if (e.code == "ArrowUp"){
-                dir.y = -1;
+            if (e.code == "PageUp" || e.code == "PageDown"){
+                const dir = e.code == "PageUp" ? -1 : 1;
+                const byteDiff = dir * this.editor.rendering.layout.viewportRowCount * this.editor.rendering.layout.bytesPerRow;
+                cursorByteDiff = byteDiff;
+                scrollByteDiff = byteDiff;
             }
-            if (e.code == "ArrowDown"){
-                dir.y = 1;
+            if (e.code == "Home" || e.code == "End"){
+                const currentCursorPosition = this.editor.selection.cursorPosition;
+                const currentScrollPosition = this.editor.scroll.positionInFile;
+                let newPosition = currentCursorPosition;
+                if (e.code == "Home") {
+                    newPosition = 0;
+                }
+                else if (e.code == "End") {
+                    newPosition = this.editor.data.provider.size - 1;
+                }
+                cursorByteDiff = newPosition - currentCursorPosition;
+                scrollByteDiff = newPosition - currentScrollPosition;
             }
-            if (e.code == "ArrowLeft"){
-                dir.x = -1;
-            }
-            if (e.code == "ArrowRight"){
-                dir.x = 1;
-            }
-            const diff = dir.y * this.editor.rendering.layout.bytesPerRow + dir.x;
-            if (diff != 0){
+            if (cursorByteDiff != 0){
                 if (this.editor.selection.isSelecting() && !e.shiftKey){
                     this.editor.selection.endRange();
-                    this.editor.rendering.redraw();
                 } else {
                     if (e.shiftKey){
                         if (!this.editor.selection.isSelecting()){
-                            this.editor.selection.startRange("keyboard",this.editor.selection.cursorPosition,true);
-                            this.editor.selection.hoverOverByte("keyboard",this.editor.selection.cursorPosition + diff);
+                            this.editor.selection.startRange("keyboard",this.editor.selection.cursorPosition, true);
+                            this.editor.selection.hoverOverByte("keyboard",this.editor.selection.cursorPosition + cursorByteDiff);
                         } else {
-                            this.editor.selection.hoverOverByte("keyboard",this.editor.selection.cursorPosition + diff);
+                            this.editor.selection.hoverOverByte("keyboard",this.editor.selection.cursorPosition + cursorByteDiff);
                         }
-                        this.editor.rendering.redraw();
                     }
                 }
-                
-                this.editor.selection.setCursor(this.editor.selection.cursorPosition + diff);
+                this.editor.scroll.updateScrollIndex(this.editor.scroll.positionInFile + scrollByteDiff);
+                this.editor.selection.setCursor(this.editor.selection.cursorPosition + cursorByteDiff);
                 this.editor.rendering.redraw();
                 e.preventDefault();
             }
